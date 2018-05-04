879fc98909a07b222818693333cdf898e998df06
Michael J. Huber Jr <info@op131.com>
2018-05-04 13:26:13 -0600

    Fixed an error with abstraction with the MapDataSource

--- MapComponent/Sources/MapComponentTypes.swift
+++ MapComponent/Sources/MapComponentTypes.swift
@@ -26,8 +26,14 @@ protocol MapEvents: class {
 }
 
 // The app that uses the MapComponent will need a translator that returns a MapDataSource object to pass to the coordinator
+// MapDataSource is just an array of MapDataObjects. The translator should convert the array of whatever object the app uses convert
+// them to MapDataObjects then bundle them back up as a MapDataSource.
 
 public protocol MapDataSource {
+    var dataSource: [MapDataObject] { get }
+}
+
+public protocol MapDataObject {
     
     var id: String { get }
     var title: String { get }
